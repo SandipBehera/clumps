77c4d199a7948756ff6aa189a9749c1d18d1a7c0
sandip <sandip@nuvay.digital>
2023-06-06 16:27:29 +0530

    added nwe pages

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "contact": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "clumpsContact": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServiceComponent } from './service/service.component';
+import { ProductsComponent } from './products/products.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    AboutUsComponent,
+    ServiceComponent,
+    ProductsComponent,
+    BlogsComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
