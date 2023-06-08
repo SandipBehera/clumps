ddc0d1552b1d3ca45cfe045a17c671ffc84776e6
sandip <sandip@nuvay.digital>
2023-06-08 12:54:38 +0530

    fix alignment issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clumps';
+  constructor() { 
+    this.loadJsFile("../assets/frontent_files/js/main.js"); 
+  }
+  loadJsFile(url: any): void {  
+    let node = document.createElement('script');  
+    node.src = url;  
+    node.type = 'text/javascript';  
+    document.getElementsByTagName('head')[0].appendChild(node);  
+  } 
 }
