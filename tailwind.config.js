4f9a7cbf2ba999a892f92d9940e608b2991ee8da
Rudhiranjan2001 <silicon.rudhi563@gmail.com>
2023-08-03 21:02:49 +0530

    web dev media work commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xsm':'370px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
